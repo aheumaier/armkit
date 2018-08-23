2b26d47eda01ed212f19824136034bef8f40e00c
Andreas Heumaier <mail@andreasheumaier.de>
2018-08-23 16:02:34 +0200

    Update coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,22 @@
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter 'test'
+    command_name 'Minitest'
+  end
+end
+
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter 'test'
+    command_name 'Minitest'
+  end
+end
+
+
+
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require 'simplecov'
-SimpleCov.start
 
 require "armkit"
 
